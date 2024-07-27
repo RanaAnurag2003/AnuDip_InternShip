416411995fc5ff1d234d3df7c689dc26750eee58
RanaAnurag2003 <rana.anurag2003@gmail.com>
2024-07-28 02:08:13 +0530

    Replaced files

---
+++ LAB Work/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+def count_and_display_words_starting_with_i(file_path):
+#     count = 0
+    
+#     with open(file_path, 'r') as file:
+#             for line in file:
+#                 words = line.split()
+#                 for word in words:
+#                     if word.startswith('I'):
+#                         print(word)
+#                         count += 1
+        
+#     print(f"Total number of words starting with 'I': {count}")
+
+# file_path4 = "C:/Users/anura/Downloads/Data-visualization-main/Data-visualization-main/Python/20july-LabAssignment/Word.txt"  
+# count_and_display_words_starting_with_i(file_path)
